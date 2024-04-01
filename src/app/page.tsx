34493c1a04910bacf3631bdb970bb4ca820d42b7
DahianaMG <122055480+DahianaMG@users.noreply.github.com>
2024-03-31 21:39:38 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { LoginForm } from "@/components/login-form";
 export default function Home() {
   return (
     <main className="">
-      <div className="bg-fondo2 bg-cover flex items-center justify-center h-screen p-2">
+      <div className="bg-fondo2 bg-cover flex items-center justify-center p-2 flex-col justify-items-stretch min-h-screen">
         <div className="bg-transparent flex items-center justify-center h-screen">
           <div className="bg-transparent flex flex-col items-center gap-4 p-4 w-[400px] border-2 border-solid border-lime" style={{borderRadius: "15px", backdropFilter: "blur(30px)"}}>
             <img src="/rickmortylogin.png" width={200} height={200} alt="" />
